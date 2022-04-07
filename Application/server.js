3aaa15a853a56698363b618c918eb7a6622b8348
aSehat <47791263+aSehat@users.noreply.github.com>
2022-04-07 00:56:30 -0400

    Update server.js
    Reverting back to port 5000

--- Application/server.js
+++ Application/server.js
@@ -17,6 +17,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/chat', require('./routes/api/chat'));
 app.use('/api/message', require('./routes/api/message'));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
