9cb28c622e327def9600031ddd2536e86a75f5f6
shlokshah-dev <shlokshah18@gmail.com>
2022-04-08 21:50:55 -0400

    Chages to make Chat Real Time

--- Application/server.js
+++ Application/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { use } = require('./routes/api/users');
 
 const app = express();
 
@@ -15,9 +16,44 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/chat', require('./routes/api/chat'));
-app.use('/api/availability', require('./routes/api/availability'));
 app.use('/api/message', require('./routes/api/message'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+
+    // Creating a new Socket that will take user data from 
+    // client and would create a 'room'. So that each user joins their
+    // own room.
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        socket.emit('connected');
+    });
+
+    socket.on('join chat', (room) => {
+        socket.join(room);
+        console.log("User joined Room: " + room);
+    });
+
+    socket.on('new message', (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if (!chat.users) return console.log('Chat.users not defiend');
+
+        chat.users.forEach(user => {
+            if (user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user._id).emit("message received", newMessageReceived);
+        });
+    });
+});
